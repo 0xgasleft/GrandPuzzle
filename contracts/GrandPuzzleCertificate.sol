b83bdce584ba319dc312e75e854c1abe0b24621a
0xgasleft <0x.web3.tech@gmail.com>
2025-05-17 23:50:15 +0200

    Update certificate contract & task implementation

--- contracts/GrandPuzzleCertificate.sol
+++ contracts/GrandPuzzleCertificate.sol
@@ -9,23 +9,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GrandPuzzleCertificate is ERC721, Ownable {
     
-    address[] public requiredPieces;
+    struct Shard {
+        address shardAddress;
+        uint256 tokenId;
+    }
+
+    address[] public requiredShards;
     uint public supply;
     uint public mint_fee;
+
     bool public status;
     string public baseTokenURI;
 
     mapping (address => bool) public hasMintedCertificate;
+    mapping (address => mapping (uint => bool)) public hasBeenUsedShard;
 
     event MintFeeUpdated(uint indexed _new_fee);
     event BaseURIUpdated(string indexed _baseTokenURI);
-    event RequiredPiecesUpdated();
+    event RequiredShardsUpdated();
     event StatusUpdated(bool indexed _status);
 
 
-    constructor(string memory _name, string memory _symbol, uint _mint_fee, address[] memory _requiredPieces) ERC721(_name, _symbol) Ownable(msg.sender)
+    constructor(string memory _name, string memory _symbol, uint _mint_fee, address[] memory _requiredShards) ERC721(_name, _symbol) Ownable(msg.sender)
     {
-        requiredPieces = _requiredPieces;
+        requiredShards = _requiredShards;
         mint_fee = _mint_fee;
         status = false;
     }
@@ -40,14 +47,17 @@ contract GrandPuzzleCertificate is ERC721, Ownable {
         emit MintFeeUpdated(_mint_fee);
     }
 
-    function updateRequiredPieces(address[] memory _requiredPieces) external onlyOwner {
-        requiredPieces = _requiredPieces;
-        emit RequiredPiecesUpdated();
+    function updateRequiredShards(address[] calldata _requiredShards) external onlyOwner {
+        requiredShards = _requiredShards;
+        emit RequiredShardsUpdated();
     }
 
-    function addRequiredPiece(address _piece) external onlyOwner {
-        requiredPieces.push(_piece);
-        emit RequiredPiecesUpdated();
+    function addRequiredShards(address[] calldata _requiredShards) external onlyOwner {
+        for(uint256 i = 0; i < _requiredShards.length;) {
+            requiredShards.push(_requiredShards[i]);
+            unchecked { ++i; }
+        }
+        emit RequiredShardsUpdated();
     }
 
     function setBaseURI(string memory _baseTokenURI) public onlyOwner {
@@ -65,14 +75,14 @@ contract GrandPuzzleCertificate is ERC721, Ownable {
         return supply;
     }
 
-    function mint(address to) external payable {
+    function mint(address _to, Shard[] calldata _shards) external payable {
         require(status, "Minting is not enabled");
-        require(_hasAllPieces(to), "User does not have all required pieces");
-        require(!hasMintedCertificate[to], "User has already minted a certificate");
+        require(!hasMintedCertificate[_to], "User has already minted a certificate");
         require(msg.value >= mint_fee, "Not enough native to cover mint fee");
+        _hasAllShardsAndUnused(_to, _shards);
 
-        hasMintedCertificate[to] = true;
-        _mint(to, supply);
+        hasMintedCertificate[_to] = true;
+        _mint(_to, supply);
         supply++;
     }
 
@@ -80,15 +90,32 @@ contract GrandPuzzleCertificate is ERC721, Ownable {
         return baseTokenURI;
     }
 
-    function _hasAllPieces(address user) internal view returns (bool) {
-        for (uint256 i = 0; i < requiredPieces.length;) {
-            if (IERC721(requiredPieces[i]).balanceOf(user) == 0) {
-                return false;
+    function _hasAllShardsAndUnused(address _to, Shard[] calldata _shards) internal {
+        require(_shards.length == requiredShards.length, "Incorrect number of shards");
+
+        for(uint256 i = 0; i < requiredShards.length;) {
+            
+            bool foundShard = false;
+
+            for(uint256 j = 0; j < _shards.length;) {
+                if(requiredShards[i] == _shards[j].shardAddress) {
+                    
+                    require(IERC721(_shards[j].shardAddress).ownerOf(_shards[j].tokenId) == _to, "Not owner of shard");
+                    require(!hasBeenUsedShard[_shards[j].shardAddress][_shards[j].tokenId], "Shard has already been used");
+
+                    foundShard = true;
+                    hasBeenUsedShard[_shards[j].shardAddress][_shards[j].tokenId] = true;
+
+                    break;
+                }
+                unchecked { ++j; }
             }
-            unchecked { ++i; }
+            require(foundShard, "Missing required shard");
+            unchecked { ++i; }       
         }
-        return true;
     }
+
+
 
     function withdraw(address _to) external onlyOwner {
         require(_to != address(0), "Cannot withdraw to zero address!");
